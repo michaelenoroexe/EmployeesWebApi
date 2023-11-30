316e822dde346b771d93b0dc4136e3a6131409ea
Michaelenoroexe <serezha.alexandrovitch2018@yandex.ru>
2023-11-30 21:36:17 +0300

    Added validator to service list.

--- EmployeesAPI/Program.cs
+++ EmployeesAPI/Program.cs
@@ -1,11 +1,15 @@
+using EmployeesAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<ValidationFilterAttribute>();
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
